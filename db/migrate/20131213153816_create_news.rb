e0ca76922482c9cb312d9115755b97dbffeef18b
dextract <dextract@gmail.com>
2013-12-14 22:10:54 +0000

    Search refined

--- db/migrate/20131213153816_create_news.rb
+++ db/migrate/20131213153816_create_news.rb
@@ -1,7 +1,14 @@
 class CreateNews < ActiveRecord::Migration
   def change
-    add_column :news, :content, :string
-    add_column :news, :user_id, :integer
+
+    create_table :news do |t|
+      t.string :title
+      t.string :photo
+      t.integer :user_id
+      t.string :content
+      t.timestamps
+    end
+
     add_index :news, [:user_id,:created_at]
   end
 end
