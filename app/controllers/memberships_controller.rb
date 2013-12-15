ada7eb89c14896c7f1b9651b7bcc14f6a1a81e27
dextract <dextract@gmail.com>
2013-12-15 22:46:52 +0000

    fixes

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -1,16 +1,17 @@
 class MembershipsController < ApplicationController
   def destroy
-     Membership.find_by_user_id(params[:user_id]).destroy
+     Membership.find_by_user_id(params[:user_id]).update_attribute(:ex, 'true')
+     Company.find(params[:company]).memberships.find_by_user_id(params[:user_id]).update_attribute(:ex, 'true')
      redirect_to company_path(params[:company])
      flash[:success] = "Colaborador removido"
   end
 
   def remove_pres
     Membership.find_by_user_id(params[:user_id]).update_attribute(:owner, 'false')
+    Company.find(params[:company]).memberships.find_by_user_id(params[:user_id]).update_attribute(:owner, 'false')
     redirect_to user_path(params[:user_id])
     flash[:success] = "Presidência removida"
   end
-
 
 
   private
